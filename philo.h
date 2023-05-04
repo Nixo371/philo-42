383071be5579923b94c70c50da32fcfb68fb8e92
Nixo371 <nicolas.ucieda@gmail.com>
2023-05-04 23:48:20 +0200

    Kinda working, definitely some things to work on

--- philo.h
+++ philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nucieda <nucieda@student.42.fr>            +#+  +:+       +#+        */
+/*   By: nucieda- <nucieda-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 20:13:53 by nucieda           #+#    #+#             */
-/*   Updated: 2023/05/04 21:24:40 by nucieda          ###   ########.fr       */
+/*   Updated: 2023/05/04 23:25:43 by nucieda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # include <stdio.h>
 # include <string.h>
 # include <pthread.h>
+# include <sys/time.h>
 
 typedef struct s_philo
 {
@@ -25,6 +26,7 @@ typedef struct s_philo
 	int				last_eat;
 	int				last_sleep;
 	int				meals;
+	int				dead;
 	pthread_t		th;
 	pthread_mutex_t *left;
 	pthread_mutex_t *right;
--- philosophers.c
+++ philosophers.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philosophers.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nucieda <nucieda@student.42.fr>            +#+  +:+       +#+        */
+/*   By: nucieda- <nucieda-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 20:13:39 by nucieda           #+#    #+#             */
-/*   Updated: 2023/05/04 21:44:14 by nucieda          ###   ########.fr       */
+/*   Updated: 2023/05/04 23:47:15 by nucieda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,12 +38,14 @@ t_philo *philo_init(t_table *table)
 	i = 0;
 	while(i < table->count)
 	{
-		philo->id = i + 1;
-		philo->last_eat = 0;
-		philo->last_sleep = 0;
-		philo->left = get_fork(table, philo->id, 'L');
-		philo->right = get_fork(table, philo->id, 'R');
-		philo->meals = 0;
+		philo[i].id = i + 1;
+		philo[i].last_eat = 0;
+		philo[i].last_sleep = 0;
+		philo[i].left = get_fork(table, philo->id, 'L');
+		philo[i].right = get_fork(table, philo->id, 'R');
+		philo[i].meals = table->meals;
+		philo[i].dead = 0;
+		i++;
 	}
 	return (philo);
 }
@@ -59,6 +61,7 @@ pthread_mutex_t	*forks_init(t_table *table)
 	{
 		if(pthread_mutex_init(&forks[i], NULL) != 0)
 			return (NULL);
+		i++;
 	}
 	return (forks);
 }
@@ -72,6 +75,7 @@ t_table	*table_init(char *argv[])
 	table->die = atoi(argv[2]);
 	table->eat = atoi(argv[3]);
 	table->sleep = atoi(argv[4]);
+	table->meals = -1;
 	if (argv[5])
 		table->meals = atoi(argv[5]);
 	table->forks = forks_init(table);
@@ -79,14 +83,158 @@ t_table	*table_init(char *argv[])
 	return (table);
 }
 
-void	start_sim(t_table *table)
+long int	timeval_to_ms(struct timeval time)
+{
+	long int	ms;
+
+	ms = 0;
+
+	ms += time.tv_sec * 1000;
+	ms += time.tv_usec / 1000;
+	return (ms);
+}
+
+int	check_delay(struct timeval timer)
+{
+	struct timeval	new;
+	long int		start;
+	long int		now;
+
+	gettimeofday(&new, NULL);
+	start = timeval_to_ms(timer);
+	now = timeval_to_ms(new);
+	return (now - start);
+}
+
+void	p_print(t_philo philo, char *s, int	ms)
+{
+	printf("%d\t%d %s", ms, philo.id, s);
+}
+
+int		check_dead(t_table *table)
 {
 	int	i;
 
 	i = 0;
 	while (i < table->count)
 	{
-		
+		if (table->philos[i].dead)
+			return(1);
+		i++;
+	}
+	return (0);
+}
+
+int	check_death(t_philo *philo, int time_to_die, int time)
+{
+	if (philo->last_eat > time_to_die)
+	{
+		p_print(*philo, "died\n", time);
+		philo->dead = 1;
+		return (1);
+	}
+	return (0);
+}
+
+int	p_eat(t_philo *philo, int time_to_eat, int	time_to_die, int time)
+{
+	struct timeval	timer;
+	int				clock;
+	
+	gettimeofday(&timer, NULL);
+	pthread_mutex_lock(philo->left);
+	clock = check_delay(timer);
+	philo->last_eat += clock;
+	if (check_death(philo, time_to_die, clock + time))
+		return (clock - time);
+	p_print(*philo, "has taken a fork\n", clock + time);
+	pthread_mutex_lock(philo->right);
+	philo->last_eat -= clock;
+	clock = check_delay(timer);
+	philo->last_eat += clock;
+	if (check_death(philo, time_to_die, clock + time))
+		return (clock - time);
+	p_print(*philo, "has taken a fork\n", clock + time);
+	philo->last_eat -= clock;
+	clock = check_delay(timer);
+	philo->last_eat += clock;
+	if (check_death(philo, time_to_die, clock + time))
+		return (clock - time);
+	p_print(*philo, "is eating\n", clock + time);
+	usleep(time_to_eat * 1000);
+	pthread_mutex_unlock(philo->left);
+	pthread_mutex_unlock(philo->right);
+	philo->last_eat = 0;
+	clock = check_delay(timer);
+	return (clock - time);
+}
+
+int	p_sleep(t_philo *philo, int	time_to_sleep, int time)
+{
+	struct timeval	timer;
+	int				clock;
+
+	gettimeofday(&timer, NULL);
+	clock = check_delay(timer);
+	p_print(*philo, "is sleeping\n", clock + time);
+	usleep(time_to_sleep * 1000);
+	philo->last_eat += time_to_sleep;
+	p_print(*philo, "is thinking\n", clock + time + time_to_sleep);
+	clock = check_delay(timer);
+	return (clock - time);	
+}
+
+void	*exist(void	*arg)
+{
+	t_table	*table;
+	struct	timeval timer;
+	int		id;
+	int		clock;
+	int		offset;
+
+	id = **(int **)arg;
+	table = ((t_table **)arg)[1];
+	gettimeofday(&timer, NULL);
+	offset = -1;
+	if (table->philos[id].meals == -1)
+		offset = 0;
+	while(table->philos[id].meals != 0)
+	{
+		clock = check_delay(timer);
+		if (check_dead(table))
+			break;
+		p_eat(&table->philos[id], table->eat, table->die, clock);
+		clock = check_delay(timer);
+		if (check_dead(table))
+			break;
+		p_sleep(&table->philos[id], table->sleep, clock);
+		table->philos[id].meals += offset;
+	}
+	free(*(void **)arg);
+	free(arg);
+	return (NULL);
+}
+
+void	start_sim(t_table *table)
+{
+	void	**id;
+	int	i;
+
+	i = 0;
+	while (i < table->count)
+	{
+		id = malloc(2 * sizeof(void *));
+		id[0] = malloc(sizeof(int));
+		*(int *)id[0] = i;
+		id[1] = (void *)table;
+		if (pthread_create(&table->philos[i].th, NULL, &exist, id) != 0)
+			exit(5);
+		i++;
+	}
+	while (--i >= 0)
+	{
+		if (pthread_join(table->philos[i].th, NULL) != 0)
+			exit(6);
 	}
 }
 
